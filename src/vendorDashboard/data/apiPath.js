3db0fcf0f166a201772aedbc067204da9407708a
Pasupuleti Harshavardhini <pasupuletiharshavardhini271@gmail.com>
2025-03-15 20:56:02 +0530

    made changes in data/path to backend-onrender.com

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,5 +1,5 @@
 
 
-export const API_URL = "http://localhost:5500"
+// export const API_URL = "http://localhost:5500"
 
-// const API_URL = "https://zaaya-exclusive-for-women-backend.onrender.com"
+const API_URL = "https://zaaya-exclusive-for-women-backend.onrender.com"
